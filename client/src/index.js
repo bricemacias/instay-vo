c0b32ef32915bcf08b0abbd7508404aaf671b43b
Brice Mvf <bricemacias@hotmail.com>
2020-02-16 15:41:53 +0100

    Apollo client uri changed

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4444/graphql'
+  uri: 'https://instay-v0.herokuapp.com/graphql'
 });
 
 ReactDOM.render(
